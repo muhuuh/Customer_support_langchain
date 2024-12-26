3a567b4428f61f7a152d8cb18da0a67e88af1036
arani <aranimontes@protonmail.com>
2024-12-26 22:05:57 +0100

    cleaned file to remove not needed code

--- main.py
+++ main.py
@@ -1,55 +1,18 @@
 # Note: Replace **<YOUR_APPLICATION_TOKEN>** with your actual Application token
-
-import argparse
-import json
-from argparse import RawTextHelpFormatter
 import requests
-from typing import Optional
-import warnings
-try:
-    from langflow.load import upload_file
-except ImportError:
-    warnings.warn("Langflow provides a function to help you upload files to the flow. Please install langflow to use it.")
-    upload_file = None
+import os
+
+
 
 BASE_API_URL = "https://api.langflow.astra.datastax.com"
 LANGFLOW_ID = "baf5b460-1478-4a11-90c7-84a4ea5036c6"
 FLOW_ID = "982d4184-8457-46c2-b2e1-ab85051a7446"
-APPLICATION_TOKEN = "<YOUR_APPLICATION_TOKEN>"
+APPLICATION_TOKEN = os.environ.get("APP_TOKEN")
 ENDPOINT = "customer_test" # The endpoint name of the flow
 
-# You can tweak the flow by adding a tweaks dictionary
-# e.g {"OpenAI-XXXXX": {"model_name": "gpt-4"}}
-TWEAKS = {
-  "ChatInput-g2EJV": {},
-  "ChatOutput-cvim1": {},
-  "Agent-ebYqk": {},
-  "Prompt-9lo6M": {},
-  "AstraDB-hsw3G": {},
-  "ParseData-LmWtN": {},
-  "File-kmK2F": {},
-  "SplitText-vm6Lq": {},
-  "AstraDB-07sYn": {},
-  "Agent-dNaS0": {},
-  "Agent-pAVSB": {},
-  "AstraDBToolComponent-SnYYB": {},
-  "AstraDBToolComponent-GlCNE": {}
-}
 
-def run_flow(message: str,
-  endpoint: str,
-  output_type: str = "chat",
-  input_type: str = "chat",
-  tweaks: Optional[dict] = None,
-  application_token: Optional[str] = None) -> dict:
-    """
-    Run a flow with a given message and optional tweaks.
-
-    :param message: The message to send to the flow
-    :param endpoint: The ID or the endpoint name of the flow
-    :param tweaks: Optional tweaks to customize the flow
-    :return: The JSON response from the flow
-    """
+def run_flow(message: str) -> dict:
+   
     api_url = f"{BASE_API_URL}/lf/{LANGFLOW_ID}/api/v1/run/{endpoint}"
 
     payload = {
@@ -65,42 +28,3 @@ def run_flow(message: str,
     response = requests.post(api_url, json=payload, headers=headers)
     return response.json()
 
-def main():
-    parser = argparse.ArgumentParser(description="""Run a flow with a given message and optional tweaks.
-Run it like: python <your file>.py "your message here" --endpoint "your_endpoint" --tweaks '{"key": "value"}'""",
-        formatter_class=RawTextHelpFormatter)
-    parser.add_argument("message", type=str, help="The message to send to the flow")
-    parser.add_argument("--endpoint", type=str, default=ENDPOINT or FLOW_ID, help="The ID or the endpoint name of the flow")
-    parser.add_argument("--tweaks", type=str, help="JSON string representing the tweaks to customize the flow", default=json.dumps(TWEAKS))
-    parser.add_argument("--application_token", type=str, default=APPLICATION_TOKEN, help="Application Token for authentication")
-    parser.add_argument("--output_type", type=str, default="chat", help="The output type")
-    parser.add_argument("--input_type", type=str, default="chat", help="The input type")
-    parser.add_argument("--upload_file", type=str, help="Path to the file to upload", default=None)
-    parser.add_argument("--components", type=str, help="Components to upload the file to", default=None)
-
-    args = parser.parse_args()
-    try:
-      tweaks = json.loads(args.tweaks)
-    except json.JSONDecodeError:
-      raise ValueError("Invalid tweaks JSON string")
-
-    if args.upload_file:
-        if not upload_file:
-            raise ImportError("Langflow is not installed. Please install it to use the upload_file function.")
-        elif not args.components:
-            raise ValueError("You need to provide the components to upload the file to.")
-        tweaks = upload_file(file_path=args.upload_file, host=BASE_API_URL, flow_id=ENDPOINT, components=args.components, tweaks=tweaks)
-
-    response = run_flow(
-        message=args.message,
-        endpoint=args.endpoint,
-        output_type=args.output_type,
-        input_type=args.input_type,
-        tweaks=tweaks,
-        application_token=args.application_token
-    )
-
-    print(json.dumps(response, indent=2))
-
-if __name__ == "__main__":
-    main()
